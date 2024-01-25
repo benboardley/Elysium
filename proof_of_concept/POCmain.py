5e7dda4e6895f4a17efa4f534e0692381d77d3f2
Garrett Phillips <gphill1030@gmail.com>
2024-01-25 15:55:38 -0500

    updated main to work with spotify class

--- proof_of_concept/POCmain.py
+++ proof_of_concept/POCmain.py
@@ -1,41 +1,56 @@
 import os
-import POCspotify
-import POCapplmus
+from dotenv import load_dotenv
+
+from POCspotify import POCspotify
+from POCapplmus import POCapplmus
 
 ### Set Up Environment variables ###
 def setup_env():
     print("Set Up Environment variables")
+    #load_dotenv(dotenv_path='../keys.env', verbose=True)
 
 
 ### Authorization of Spotify ###
 def spot_auth():
-    
+    spotify = POCspotify()
+    spotify.request_auth()
+    if spotify.sp:
+        spotify.top_ten_tracks()
 
 
 ### Authorization of Apple Music ###
-def spot_auth():
+def applmus_auth():
     print("Authorization of Apple Music")
 
 
 ### Request transfer service type ###
 def transfer_service():
-    streaming_service = input("""Input either 0 or 1 to transfer\n
-                              (0) Spotify -> Apple Music\n(1) Apple 
-                              Music -> Spotify\nSelection: """)
+    streaming_service = input(
+        "\nInput either 0 or 1 to transfer\n"
+        "(0) Spotify -> Apple Music\n"
+        "(1) Apple Music -> Spotify\n"
+        "Selection: "
+    )
     return streaming_service
 
 
 ### Request transfer type ###
 def transfer_type():
-    trans_type = input("""Input either 0 or 1 for type to transfer\n
-                              (0) Song\n(1) Playlist\nSelection: """)
+    trans_type = input(
+        "Input either 0 or 1 for type to transfer\n"
+        "(0) Song\n"
+        "(1) Playlist\n"
+        "Selection: "
+    )
     return trans_type
 
 
 if __name__ == '__main__':
     ### Set Up Environment variables ###
+    setup_env()
 
     ### Authorization of Spotify ###
+    spot_auth()
 
     ### Authorization of Apple Music ###
 
--- proof_of_concept/POCspotify.py
+++ proof_of_concept/POCspotify.py
@@ -1,5 +1,5 @@
-import spotipy
 import os
+import spotipy
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -7,11 +7,13 @@ class POCspotify:
     ### POCspotify class variables ###
 
     def __init__(self):
+        load_dotenv(dotenv_path='../keys.env', verbose=True)
         self.client_id = os.environ["client_id"]
         self.client_secret = os.environ["client_secret"]
         self.redirect_uri = 'http://localhost:3000/callback'
         self.scope = 'user-library-read user-top-read' # Take as argument l8r
         self.sp = None
+
 
     def request_auth(self):
         sp_oauth = SpotifyOAuth(
@@ -29,27 +31,22 @@ class POCspotify:
 
         self.sp = spotipy.Spotify(auth=access_token)
 
+    
+    def top_ten_tracks(self):
+        top_tracks = self.sp.current_user_top_tracks(limit=10)
 
+        print("Your top tracks:")
+        for track in top_tracks['items']:
+            print(f"- {track['name']} by {track['artists'][0]['name']}")
 
-load_dotenv(dotenv_path='../keys.env', verbose=True)
-spotify = POCspotify()
-spotify.request_auth()
-
-if spotify.sp:
-    top_tracks = spotify.sp.current_user_top_tracks(limit=10)
-
-    print("Your top tracks:")
-    for track in top_tracks['items']:
-        print(f"- {track['name']} by {track['artists'][0]['name']}")
-
-    results = spotify.sp.search(q='artist:Lil Wayne track:Mona Lisa', type='track')
-    track_uri = results['tracks']['items'][0]['uri']
-    audio_features = spotify.sp.audio_features(track_uri)[0]
-    # Get more details about the track
-    print(f"Name: {results['tracks']['items'][0]['name']}")
-    print(f"Artist: {results['tracks']['items'][0]['artists'][0]['name']}")
-    print(f"Album: {results['tracks']['items'][0]['album']['name']}")
-    print(f"Popularity: {results['tracks']['items'][0]['popularity']}")
-    print(f"Energy: {audio_features['energy']}")
-    print(f"Danceability: {audio_features['danceability']}")
-    print(f"Duration: {audio_features['duration_ms']} milliseconds")
+        results = self.sp.search(q='artist:Lil Wayne track:Mona Lisa', type='track')
+        track_uri = results['tracks']['items'][0]['uri']
+        audio_features = self.sp.audio_features(track_uri)[0]
+        # Get more details about the track
+        print(f"Name: {results['tracks']['items'][0]['name']}")
+        print(f"Artist: {results['tracks']['items'][0]['artists'][0]['name']}")
+        print(f"Album: {results['tracks']['items'][0]['album']['name']}")
+        print(f"Popularity: {results['tracks']['items'][0]['popularity']}")
+        print(f"Energy: {audio_features['energy']}")
+        print(f"Danceability: {audio_features['danceability']}")
+        print(f"Duration: {audio_features['duration_ms']} milliseconds")
