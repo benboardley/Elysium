d5250471bf6a548b25d9cfba1b85232c289e4a0a
Ben Boardley <bboardle@purdue.edu>
2024-01-25 08:01:59 -0500

    song object created

--- proof_of_concept/POCsong.py
+++ proof_of_concept/POCsong.py
@@ -1,12 +1,13 @@
 class POCsong:
     ### POCspotify class variables ###
-    def __init__(self):
-        self.name = None
-        self.artist = None
-        self.artist_features = []
-        self.origin = None
-        self.uri = None
-        self.audio_features = None
+    def __init__(self, track, audio_features, origin=None):
+        self.name = track['track']['name']
+        self.artist = track['track']['album']['artists'][0]['name']
+        self.artist_features = [artist['name'] for artist in track['track']['album']['artists'][1:] if len(track['track']['album']['artists']) > 0]
+        self.artist_features.extend([artist['name'] for artist in track['track']['artists'] if artist['name'] not in self.artist])
+        self.origin = origin
+        self.uri = track['track']['uri']
+        self.audio_features = audio_features
         self.other_available_platforms = []
-        self.song_clip_location = None
-        self.song_thumbnail = None
+        self.song_clip_location = track['track']['preview_url']
+        self.song_thumbnail_location = None#track['track']['video_thumbnail']['url']
