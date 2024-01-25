a4fe96413fa28790ead38a31085177a9f2f46a81
Ben Boardley <bboardle@purdue.edu>
2024-01-25 08:03:13 -0500

    create testing folder

---
+++ testing/test_POCspotify.py
@@ -0,0 +1,9 @@
+import unittest
+
+from proof_of_concept.POCspotify import POCspotify
+
+class TestMyModule(unittest.TestCase):
+    
+    def test_example(self):
+        result = your_function_to_test()
+        self.assertEqual(result, expected_value)
