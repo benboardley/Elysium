be47f50af50df8fde770e870b7e4477852d206f4
Jack Douglas Gardner <gardne97@eceprog1.ecn.purdue.edu>
2024-01-31 16:38:29 -0500

    New Setup File

--- Elysium/setup.sh
+++ Elysium/setup.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
+# Create a virtual environment (optional but recommended)
+python3 -m venv venv
+. venv/bin/activate
+
 # Install pre-commit
 pip install pre-commit
 
-# Create a virtual environment (optional but recommended)
-python -m venv venv
-source venv/bin/activate
-
 # Install linters and dependencies
 pip install flake8 pylint
-npm install eslint eslint-config-airbnb-base
+npm install eslint eslint-config-airbnb-base prettier
 
 # Create pre-commit configuration file
-echo "- repo: https://github.com/pre-commit/pre-commit-hooks" > .pre-commit-config.yaml
-echo "  rev: v3.4.0" >> .pre-commit-config.yaml
-echo "  hooks:" >> .pre-commit-config.yaml
-echo "    - id: trailing-whitespace" >> .pre-commit-config.yaml
+cat <<EOF > .pre-commit-config.yaml
+repos:
+  - repo: https://github.com/pre-commit/pre-commit-hooks
+    rev: v3.4.0
+    hooks:
+      - id: trailing-whitespace
 
-echo "- repo: https://github.com/pre-commit/mirrors-eslint" >> .pre-commit-config.yaml
-echo "  rev: v6.6.0" >> .pre-commit-config.yaml
-echo "  hooks:" >> .pre-commit-config.yaml
-echo "    - id: eslint" >> .pre-commit-config.yaml
-echo "      files: \\.js$" >> .pre-commit-config.yaml
-echo "      additional_dependencies: [eslint-config-airbnb-base]" >> .pre-commit-config.yaml
+  - repo: https://github.com/pre-commit/mirrors-prettier
+    rev: v3.1.0
+    hooks:
+      - id: prettier
+        types_or: [css, javascript]
 
-echo "- repo: https://github.com/pre-commit/mirrors-flake8" >> .pre-commit-config.yaml
-echo "  rev: v3.9.2" >> .pre-commit-config.yaml
-echo "  hooks:" >> .pre-commit-config.yaml
-echo "    - id: flake8" >> .pre-commit-config.yaml
-echo "      files: \\.py$" >> .pre-commit-config.yaml
+  - repo: https://github.com/PyCQA/flake8
+    rev: 7.0.0
+    hooks:
+      - id: flake8
+        files: \.py$
+EOF
 
 # Install pre-commit hooks
 pre-commit install
