8535dc1060bb7aee2bd6498b8a517881e67bf26b
Ben Boardley <bboardle@purdue.edu>
2024-02-13 00:00:19 -0500

    beginning user profile

--- Elysium_Backend/user/models.py
+++ Elysium_Backend/user/models.py
@@ -10,6 +10,20 @@ class CustomUser(AbstractUser):
     # first_name + last_name
     # is_active, is_staff, is_superuser
     pass
+
 class Profile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    #profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    spotify_access_token = models.CharField(max_length=50, blank=True)
+    apple_access_token = models.CharField(max_length=50, blank=True)
+    following = models.ManyToManyField(
+        "self",
+        related_name="followers",
+        symmetrical=False,
+        blank=True
+    )
+    def __str__(self):
+        return self.user.username
