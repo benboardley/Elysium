6fc0e19f084e3370e591ac94179599f59602af9b
Ben Boardley <bboardle@purdue.edu>
2024-03-18 16:25:31 -0400

    profile search

--- Elysium_Backend/social/urls.py
+++ Elysium_Backend/social/urls.py
@@ -3,6 +3,6 @@ from .api.views import *
 urlpatterns = [
     path('posts/', Posts.as_view(), name='posts'),
     path('posts/<int:id>', Posts.as_view(), name='post'),
-    path('/posts/follow/', FollowFeed.as_view(), name='follow-feed'),
+    path('posts/follow/', FollowFeed.as_view(), name='follow-feed'),
     path('public/', PublicFeed.as_view(), name='public-feed'),
 ]
