e8501895c4e3dc999c1c53d39cda197f0f4e68cc
Ben Boardley <bboardle@purdue.edu>
2024-04-22 14:36:19 -0400

    updated appspec.yml permissions

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -14,16 +14,17 @@ export PATH="/home/ubuntu/.nvm/versions/node/v18.20.2/bin:$PATH"
 cd /home/ubuntu/apps
 echo $PATH >> /home/ubuntu/path.log
 sudo apt-get clean
+sudo chown -R ubuntu:ubuntu /home/ubuntu/apps
 npm cache clean --force
 #scp ../environment-files/package.json .
-sudo scp ../environment-files/keys.env Elysium_Backend/keys.env
+scp ../environment-files/keys.env Elysium_Backend/keys.env
 cd Elysium_Backend
 pip install -r requirements.txt
-sudo python3 manage.py makemigrations >> makemigrations.log 2>&1
-sudo python3 manage.py migrate >> migrate.log 2>&1
+python3 manage.py makemigrations >> makemigrations.log 2>&1
+python3 manage.py migrate >> migrate.log 2>&1
 cd ..
 cd Elysium_Frontend
-sudo npm install >> npm-install.log 2>&1
+npm install >> npm-install.log 2>&1
 #npm install
 #rm -rf /home/ubuntu/apps/src/frontend
 #scp -r build/* /var/www/build
