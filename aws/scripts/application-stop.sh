006e074e4bc9d73af1b7be375a693d5aa41fb5e0
Ben Boardley <bboardle@purdue.edu>
2024-04-22 13:04:37 -0400

    change file permissions

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 #set -x
-# Log file path
-#log_file="/home/ubuntu/pm2.logs"
-
-# Log environment variables
-#env >> "$log_file"
 
 
-# Navigate to the application directory
-#cd /home/ubuntu/apps
-
-# Get the number of online processes without a newline
-#online_processes=$(pm2 list | grep -c 'online')
-
-# Check if there are any online processes
-#if [ "$online_processes" -gt 0 ]; then
-    # Stop PM2 processes and capture output to log file
-#    pm2 stop all >> "$log_file" 2>&1
-#    pm2 delete all >> "$log_file" 2>&1
-#else
-#    echo "No online processes to stop and delete." >> "$log_file"
-#fi
-#echo "complete"
 pkill -f 'webpack'
 pkill -f 'runserver'
