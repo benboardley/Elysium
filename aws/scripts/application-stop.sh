bf600015df3f0383c01d09658b083919edfe435a
Ben Boardley <bboardle@purdue.edu>
2024-04-23 00:39:56 -0400

    change script permissions

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -x
+set -xe
 
 export PATH="/home/ubuntu/.local/bin:/home/ubuntu/.nvm/versions/node/v18.20.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-pm2 stop frontend
-pm2 delete frontend
+pm2 stop all
+pm2 delete all
 sudo systemctl stop gunicorn.service
